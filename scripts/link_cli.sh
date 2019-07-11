a8c2669ede6768bdc919b49acc4b9fea30c4bd22
mikearbib <arbib.michael@gmail.com>
2019-07-11 23:23:38 +0300

    Added '--since develop' flag for lerna commands

--- scripts/link_cli.sh
+++ scripts/link_cli.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 echo """
+--------------------------------
+git status -->
+$(git status)
+--------------------------------
 link_cli.sh exec path --> $(pwd)
+--------------------------------
 """
 lerna exec pwd --since develop | grep cli
 if [[ "$?" -eq 0 ]]; then
